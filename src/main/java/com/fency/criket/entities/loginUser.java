48853b1492b1adf81168d8c7a6bbff9da0532fb5
Sathiyaseelan <kaandieepan.sathiyaseelan@sky.uk>
2024-02-15 11:29:51 +0530

    Login API file created

---
+++ src/main/java/com/fency/criket/entities/loginUser.java
@@ -0,0 +1,13 @@
+package com.fency.criket.entities;
+
+public class loginUser {
+
+    public String username;
+    public String password;
+
+    loginUser(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+}
